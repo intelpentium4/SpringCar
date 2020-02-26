d14e783882203c8039bd2de7bbd04c0f1f13ef96
intelnuc <intel.darren@gmail.com>
2020-02-25 20:20:37 -0500

    added db support and fixed an issue in vehicle entity

--- src/main/java/com/personalproject/springboot/cardemo/controller/VehicleController.java
+++ src/main/java/com/personalproject/springboot/cardemo/controller/VehicleController.java
@@ -1,43 +1,33 @@
 package com.personalproject.springboot.cardemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.personalproject.springboot.cardemo.entity.Vehicle;
+import com.personalproject.springboot.cardemo.service.VehicleService;
 
 @Controller
 @RequestMapping("/vehicles")
 public class VehicleController {
-
-	// load vehicle data
-	private List<Vehicle> theVehicles;
 	
-	@PostConstruct
-	private void loadData() {
-		// create Vehicles
-		Vehicle car1 = new Vehicle(1, 2020, "Toyota", "Camry", 203, 32, 24_425);
-		Vehicle car2 = new Vehicle(1, 2020, "Hyundai", "Veloster", 147, 29, 18_800);
-		Vehicle car3 = new Vehicle(1, 2019, "Toyota", "Prius", 121, 52, 24_325);
+	private VehicleService vehicleService;
 	
-		// create the list
-		theVehicles = new ArrayList<>();
-		
-		// add to the list
-		theVehicles.add(car1);
-		theVehicles.add(car2);
-		theVehicles.add(car3);
+	@Autowired
+	public VehicleController(VehicleService theVehicleService) {
+		vehicleService = theVehicleService;
 	}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
 	public String listVehicles(Model theModel) {
+		
+		// Get vehicles from the db
+		List<Vehicle> theVehicles = vehicleService.findAll();
 		
 		// add to the spring model
 		theModel.addAttribute("vehicles", theVehicles );
--- src/main/java/com/personalproject/springboot/cardemo/entity/Vehicle.java
+++ src/main/java/com/personalproject/springboot/cardemo/entity/Vehicle.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="employee")
+@Table(name="vehicle")
 public class Vehicle {
 	
 	// define fields
